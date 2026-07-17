0388c14283e41ca0bc2604e62485de9b1e70024a
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2026-07-17 14:07:04 +0800

    Pixiv 订阅：添加 书源设置

--- src/rssSource/pixiv/base.loginUrl.js
+++ src/rssSource/pixiv/base.loginUrl.js
@@ -57,3 +57,41 @@ function getCookie() {
     }
 }
 
+function putInCacheObject(objectName, object, saveSeconds) {
+    if (object === undefined) object = null
+    if (saveSeconds === undefined) saveSeconds = 0
+    cache.put(objectName, JSON.stringify(object), saveSeconds)
+}
+
+function getFromCacheObject(objectName) {
+    let object = cache.get(objectName)
+    if (object === undefined) return null  // 兼容源阅，避免 parse 报错
+    return JSON.parse(object)
+}
+
+function statusMsg(status) {
+    if (status === true) return "✅ 已开启"
+    else if (status === false) return "🚫 已关闭"
+    else if (status === undefined) return "🈚️ 未设置"
+}
+
+const settingsName = {
+    "ADD_CHAPTER_INDEX": "🔢 章节编号",
+    "AUTO_LIKE_NOVELS": "❤️ 自动收藏",
+    "COMBINE_NOVELS": "📚 整合系列",
+}
+
+function editSettings(settingName) {
+    let msg, status
+    let settings = getFromCacheObject("pixivSettings")
+    // if (!settings) settings = setDefaultSettings()
+    if (!!settings[settingName]) {
+        status = settings[settingName] = !settings[settingName]
+    } else {
+        status = settings[settingName] = true
+    }
+    putInCacheObject("pixivSettings", settings)
+    msg = `\n\n${statusMsg(status)}　${settingsName[settingName]}`
+    sleepToast(msg)
+}
+
