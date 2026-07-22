a91d4686b472ae06bb32ca04bd2c7bc6b5b2438b
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2026-07-23 01:01:41 +0800

    站点：设置动态 cnNav 1

--- doc/.vitepress/navSidebar.js
+++ doc/.vitepress/navSidebar.js
@@ -1,55 +1,9 @@
 export const BLOG = "https://downeyrem.pages.dev/"
 
-export const cnNav = [
-    { 
-        text: "🏠 主页", 
-        link: "/"
-    },
-    {
-        text: "📌 常用工具",
-        activeMatch: '/',
-        items: [
-            { text: "⬇️ 下载阅读", link: "/Download" },
-            { text: "🚀 一键导入", link: "/Import" },
-        ],
-    },
-    {
-        text: "📚 书源指南",
-        activeMatch: '/',
-        items: [
-            { text: "✨ 臻享阅读", link: "/BetterExperience", activeMatch: '/' },
-            { text: "⚡️️️ 快速开始", link: "/QuickStart" },
-            { text: "➕ 添加小说", link: "/AddNovels" },
-            { text: "🅿️ Pixiv 书源", link: "/Pixiv" },
-            { text: "🦊 Linpx 书源", link: "/Linpx" },
-            { text: "🐯 兽人小说站", link: "/FurryNovel" },
-        ],
-    },
-    {
-        text: "📖 阅读指南",
-        items: [
-            { text: "📚 软件合集", link: "/Legado" },
-            { text: "🚀 导入书源", link: "/ImportBookSource" },
-            { text: "🚀 导入订阅", link: "/ImportRssSource" },
-            { text: "📌 使用技巧", link: "/LegadoTips" },
-            { text: "💾 备份恢复", link: "/WebdavBackup" , activeMatch: '/' },
-            { text: "☁️ 远程书籍", link: "/RemoteBooks" },
-            { text: "🛠️ 故障排查", link: "/TroubleShoot" }
-        ],
-    },
-    {
-        text: "🚧 开发动态",
-        items: [
-            { text: "📜 更新日志", link: "/UpdateLog" },
-            { text: "🚀 项目进展", link: "/TimeLine" },
-            { text: "🔌 Pixiv Api", link: "/PixivWebApi" },
-            { text: "👥 开发团队", link: "/Team" },
-            { text: "🤝 友情链接", link: "/FriendLink" },
-            { text: "☕ 支持开发", link: "/Sponsor" , activeMatch: '/' },
-            { text: "✍️ 作者博客", link: BLOG },
-        ],
-    },
-]
+const homeMenu = {
+    text: "🏠 主页",
+    link: "/"
+}
 
 const toolMenu = {
     text: "常用工具",
@@ -171,6 +125,14 @@ const updateLogMenu = [
     logMenu,
     devMenu,
     sponsorMenu,
+]
+
+export const cnNav = [
+    homeMenu,
+    toolMenu,
+    newMenu,
+    legadoMenu,
+    devMenu,
 ]
 
 export let cnSidebar = {
